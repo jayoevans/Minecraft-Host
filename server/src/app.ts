6fbe02b73f7731290305aca603c6cefe36324025
jayoevans <jay.evans159@gmail.com>
2020-10-18 14:51:30 +1000

    Fixed middleware issue

--- server/src/app.ts
+++ server/src/app.ts
@@ -11,57 +11,29 @@ import { Middlewares } from "./middlewares";
 
 const app = express();
 
+app.use(express.json());
 
-//Having these in a private function doesnt work
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+
+app.use(morgan('dev'));
+app.use(helmet());
+app.use(cors());
+
 app.use("/test", test);
 app.use("/minecraft", minecraft);
 app.use("/login", login);
 app.use("/server", router);
 
-export class App
+app.use(express.static("public"));
+app.use(express.static("views"));
+
+app.use(Middlewares.notFound);
+app.use(Middlewares.errorHandler);
+
+export namespace App
 {
-    public setup()
-    {
-        console.log("In setup");
-
-        this.middleware();
-        this.routes();
-        this.assets();
-    }
-
-    private middleware()
-    {
-        console.log("In middleware");
-
-        app.use(express.json());
-
-        app.use(bodyParser.json());
-        app.use(bodyParser.urlencoded({ extended: true }));
-
-        app.use(morgan('dev'));
-        app.use(helmet());
-        app.use(cors());
-
-        app.use(Middlewares.notFound);
-        app.use(Middlewares.errorHandler);
-    }
-
-    private routes()
-    {
-        console.log("In routes");
-        app.use('/router', router);
-        app.use("/test", test);
-    }
-
-    private assets()
-    {
-        console.log("In assets");
-
-        app.use(express.static("public"));
-        app.use(express.static("views"));
-    }
-
-    public listen(port: number)
+    export function listen(port: number)
     {
         app.listen(port, () => console.log(`Running server on port ${port}...`));
     }
--- server/src/index.ts
+++ server/src/index.ts
@@ -1,7 +1,5 @@
-import {App} from "./app";
+import { App } from "./app";
 
-const app: App = new App();
 const port: number = parseInt(`${process.env.PORT}`) || 5000;
 
-app.setup();
-app.listen(port);
+App.listen(port);
